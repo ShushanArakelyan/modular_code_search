bug fixes and change f1 computation

scoring/embedder.py
@@ -83,18 +83,17 @@ class Embedder(object):
                 nt_i += 1
             rt_i += 1
         return output_tokens
-
-    @staticmethod
-    def filter_embedding_by_id(query_embedding, token_ids):
+    
+    
+    def filter_embedding_by_id(self, query_embedding, token_ids):
         token_embeddings = []
         for ti in token_ids:
-            te = []
-            for i in ti:
-                te.append(query_embedding[i])
-            te = np.mean(te, 0)
-            token_embeddings.append(te)
-        return np.asarray(token_embeddings)
+            te = torch.index_select(query_embedding, index=torch.LongTensor(ti.astype(int)).to(self.device), dim=0)
+            token_embeddings.append(torch.unsqueeze(torch.mean(te, dim=0), 0))  # i am not sure, that the gradients get propagated through here
+        token_embeddings = torch.cat(token_embeddings)
+        return token_embeddings
 
+    
     def embed_and_filter(self, doc, code, tokens_of_interest):
         # embed query and code, and get embeddings of tokens_of_interest from query, and max_len tokens from code.
         # converting the docstring and code tokens into CodeBERT inputs

scoring/eval_finetuned_codebert.py
@@ -39,7 +39,7 @@ def main():
                                  torch.nn.ReLU(),
                                  torch.nn.Linear(embedder.get_dim(), 1)).to(device)
     print(f"Evaluating checkpoint {args.checkpoint}")
-    models = torch.load(args.checkpoint)
+    models = torch.load(args.checkpoint, map_location=device)
     scorer.load_state_dict(models['scorer'])
     scorer = scorer.to(device)
     embedder.model.load_state_dict(models['embedder'])

scoring/eval_utils.py
@@ -2,7 +2,8 @@ import numpy as np
 import bleach
 import torch
 
-from scoring.eval_finetuned_codebert import run_eval_epoch
+from tqdm import tqdm
+
 from scoring.utils import extract_noun_tokens, get_ground_truth_matches
 
 
@@ -52,17 +53,8 @@ def generate_HTML(title, embedder, tokens, ground_truth_idxs, scores, split_poin
 
     true_scores_tvs = get_tokenvals(tokens, np.ones(len(tokens)), ground_truth_idxs)
     predicted_scores_tvs = get_tokenvals(tokens, scores, predicted_idxs)
-    return f'<h2>{title}</h2>{0}{1}'.format(tokensvals_to_html(embedder, predicted_scores_tvs, 0),
-                                            tokensvals_to_html(embedder, true_scores_tvs, 1))
-
-
-def find_split_point(data, scorer, embedder):
-    avg_f1 = []
-    split_points = np.arange(0, 1, 0.05)
-    for i in split_points:
-        f1_scores, precisions, recalls = run_eval_epoch(data, scorer, embedder, split_point=i)
-        avg_f1.append(np.mean(f1_scores))
-    return split_points, avg_f1
+    return '<h2>{0}</h2>{1}{2}'.format(title, tokensvals_to_html(embedder, predicted_scores_tvs, 0),
+                                       tokensvals_to_html(embedder, true_scores_tvs, 1))
 
 
 def compute_f1(ground_truth_idxs, scores, split_point):
@@ -71,12 +63,10 @@ def compute_f1(ground_truth_idxs, scores, split_point):
     S_a = len(predicted_idxs)
     intersection = len(np.intersect1d(predicted_idxs, ground_truth_idxs))
     if S_g == 0:
-        f1_score = 1 if S_a == 0 else 0
-        return f1_score, 1 if S_a == 0 else 0, 1 if S_a == 0 else 0
+        return None
     if S_a == 0:
-        P_t = 0
-    else:
-        P_t = intersection / S_a
+        return 0, 0, 0
+    P_t = intersection / S_a
     R_t = intersection / S_g
     if P_t == 0 and R_t == 0:
         f1_score = 0
@@ -103,6 +93,14 @@ def eval_example(data, it, scorer, embedder, evaluate, split_point=0.5):
     noun_tokens = extract_noun_tokens(' '.join(doc))
 
     out_tuple = embedder.embed_and_filter(doc, code, noun_tokens)
+    if out_tuple is None:
+        if evaluate == "F1":
+            result_dict['f1_scores_for_sample'] = [0]
+            result_dict['pre_for_sample'] = [0]
+            result_dict['re_for_sample'] = [0]
+        elif evaluate == "HTML":
+            result_dict['html'] = []
+        return result_dict
     noun_token_id_mapping, noun_token_embeddings, code_token_id_mapping, code_embedding, _, truncated_code_tokens = out_tuple
     for nti, nte, nt in zip(noun_token_id_mapping, noun_token_embeddings, noun_tokens):
         nte = nte.unsqueeze(0)
@@ -113,7 +111,11 @@ def eval_example(data, it, scorer, embedder, evaluate, split_point=0.5):
         forward_input = torch.cat((tiled_nte, code_embedding), dim=1)
         scorer_out = torch.sigmoid(scorer.forward(forward_input)).squeeze().cpu().detach().numpy()
         if evaluate == "F1":
-            f1, p, re = compute_f1(ground_truth_idxs, scorer_out, split_point)
+            out = compute_f1(ground_truth_idxs, scorer_out, split_point)
+            if out is None:
+                continue
+            else:
+                f1, p, re = out
             result_dict['f1_scores_for_sample'].append(f1)
             result_dict['pre_for_sample'].append(p)
             result_dict['re_for_sample'].append(re)
@@ -121,3 +123,19 @@ def eval_example(data, it, scorer, embedder, evaluate, split_point=0.5):
             result_dict['html'].append(
                 generate_HTML(nt, embedder, truncated_code_tokens, ground_truth_idxs, scorer_out, split_point))
     return result_dict
+
+
+def find_split_point(data, scorer, embedder):
+    avg_f1 = []
+    split_points = np.arange(0, 1, 0.05)
+    for i in split_points:
+        f1_scores = []
+        precisions = []
+        recalls = []
+        for it in range(len(data)):
+            result_dict = eval_example(data, it, scorer, embedder, evaluate="F1", split_point=i)
+            f1_scores.append(np.mean(result_dict['f1_scores_for_sample']))
+            precisions.append(np.mean(result_dict['pre_for_sample']))
+            recalls.append(np.mean(result_dict['re_for_sample']))
+        avg_f1.append(np.mean(f1_scores))
+    return split_points, avg_f1

scoring/finetune_codebert.py
@@ -38,7 +38,11 @@ def run_epoch(data, scorer, embedder, op, bceloss, writer, writer_epoch, device,
             op.zero_grad()
             noun_tokens = extract_noun_tokens(' '.join(doc))
             out_tuple = embedder.embed_and_filter(doc, code, noun_tokens)
+            if out_tuple is None:
+                continue
             noun_token_id_mapping, noun_token_embeddings, code_token_id_mapping, code_embedding, _, truncated_code_tokens = out_tuple
+            if noun_token_id_mapping.size == 0 or code_token_id_mapping.size == 0:
+                continue
             loss = None
             # extract positive pairs and sample negative pairs
             for nti, nte, nt in zip(noun_token_id_mapping, noun_token_embeddings, noun_tokens):
@@ -64,6 +68,7 @@ def run_epoch(data, scorer, embedder, op, bceloss, writer, writer_epoch, device,
                 id_freq_dict = {uid: c for uid, c in zip(unique_ids, counts)}
                 p = np.asarray([1 / id_freq_dict[i] for i in code[:len(code_token_id_mapping)]])
                 p = p / np.sum(p)
+                num_neg_samples = min(num_neg_samples, len(code_token_id_mapping))
                 neg_sample_idxs = np.random.choice(np.arange(len(code_token_id_mapping)),
                                                    num_neg_samples,
                                                    replace=False, p=p)
@@ -111,8 +116,10 @@ def main():
                         help='device to run on')
     parser.add_argument('--data_dir', dest='data_dir', type=str,
                         help='training data directory', required=True)
-    parser.add_argument('--valid_file_name', dest='valid_file_name', type=str,
-                        help='validation data directory', required=True)
+    parser.add_argument('--scorer_only', default=False, action='store_true')
+    parser.add_argument('--num_epochs', dest='num_epochs', type=int,
+                        help='number of epochs to train')
+
     args = parser.parse_args()
 
     device = args.device
@@ -126,12 +133,19 @@ def main():
     scorer = torch.nn.Sequential(torch.nn.Linear(embedder.get_dim() * 2, embedder.get_dim()),
                                  torch.nn.ReLU(),
                                  torch.nn.Linear(embedder.get_dim(), 1)).to(device)
-    op = torch.optim.Adam(list(scorer.parameters()) + list(embedder.model.parameters()), lr=1e-8)
+    if args.scorer_only:
+        op = torch.optim.Adam(list(scorer.parameters()), lr=1e-8)
+    else:
+        op = torch.optim.Adam(list(scorer.parameters()) + list(embedder.model.parameters()), lr=1e-8)
     bceloss = torch.nn.BCEWithLogitsLoss(reduction='sum')
 
     if args.checkpoint:
-        models = torch.load(args.checkpoint)
+        models = torch.load(args.checkpoint, map_location=device)
         checkpoint_dir = '/'.join(args.checkpoint.split('/')[:-1])
+        file_name = args.checkpoint.split('/')[-1]
+        epoch_to_start = int(file_name.split('_')[1])
+        datafile_to_start = int(file_name.split('_')[-1].split('.')[0])
+        print(f"Continuing from epoch: {epoch_to_start}, datafile: {datafile_to_start + 1}")
         scorer.load_state_dict(models['scorer'])
         print("Scorer device: ", next(scorer.parameters()).device)
         scorer = scorer.to(device)
@@ -142,12 +156,19 @@ def main():
         print("Embedder device: ", next(embedder.model.parameters()).device)
         op.load_state_dict(models['optimizer'])
     else:
+        epoch_to_start = 0
+        datafile_to_start = -1
         checkpoint_dir = f'/home/shushan/finetuned_scoring_models/{dt_string}'
         print("Checkpoints will be saved in ", checkpoint_dir)
 
         import os
         if not os.path.exists(checkpoint_dir):
             os.makedirs(checkpoint_dir)
+            
+    if args.num_epochs:
+        num_epochs = args.num_epochs
+    else:
+        num_epochs = 10
 
     train_writer_epoch = 0
 
@@ -159,14 +180,19 @@ def main():
             train_files.append(file)
     train_files = natsorted(train_files)
 
-    print('This run will not be performing validation while training')
-    for epoch in range(args.num_epochs if args.num_epochs else 5):
+    print('This run will not be performing validation while training, number of epochs to run: ', num_epochs)
+    for epoch in range(epoch_to_start, num_epochs):
         for i, input_file_name in enumerate(train_files):
+            if i < datafile_to_start:
+                continue
+            else:
+                data_file_to_start = -1
             print("Processing file: ", input_file_name)
             data = pd.read_json(input_file_name, lines=True)
             total_loss, train_writer_epoch = run_epoch(data, scorer, embedder, op, bceloss, writer, train_writer_epoch,
                                                        device, save_every=5000,
                                                        checkpoint_prefix=checkpoint_dir + f'/model_{epoch}_ep_{i}')
+        data_file_to_start = -1
 
 
 if __name__ == '__main__':