3d74e2d8973085ca6f20322e6ac6b4ae8988ba1a
shushan <arakelyan.shushan@gmail.com>
2021-09-08 14:29:13 -0700

    Update eval functions to work with phrases and CCG parses. Also add newest parameters = emb_separately and version

--- scoring/eval_finetuned_codebert.py
+++ scoring/eval_finetuned_codebert.py
@@ -9,12 +9,13 @@ from scoring.embedder import Embedder
 from scoring.eval_utils import find_split_point, eval_example
 
 
-def run_eval_epoch(data, scorer, embedder, split_point=0.5):
+def run_eval_epoch(data, scorer, embedder, embed_separately, version, split_point=0.5):
     f1_scores = []
     precisions = []
     recalls = []
     for it in tqdm(range(len(data))):
-        result_dict = eval_example(data, it, scorer, embedder, evaluate="F1", split_point=split_point)
+        result_dict = eval_example(data, it, scorer, embedder, evaluate="F1",
+                                   split_point=split_point, embed_separately=embed_separately, version=version)
         f1 = result_dict['f1_scores_for_sample']
         pre = result_dict['pre_for_sample']
         re = result_dict['re_for_sample']
@@ -28,37 +29,62 @@ def run_eval_epoch(data, scorer, embedder, split_point=0.5):
 def main():
     parser = argparse.ArgumentParser(description='Fine-tune codebert model for attend module')
     parser.add_argument('--load_from_checkpoint', dest='checkpoint', type=str,
-                        help='continue training from checkpoint', required=True)
+                        help='evaluate model saved in checkpoint')
+    parser.add_argument('--checkpoint_dir', dest='checkpoint_dir', type=str,
+                        help='evaluate model on all checkpoints in the directory')
     parser.add_argument('--device', dest='device', type=str,
                         help='device to run on', required=True)
-    parser.add_argument('--valid_file_name', dest='valid_file_name', type=str,
-                        help='validation data directory', required=True)
-    parser.add_argument('--train_file_name', dest='train_file_name', type=str,
-                        help='data to find the best split point', required=True)
+    #     parser.add_argument('--valid_file_name', dest='valid_file_name', type=str,
+    #                         help='validation data directory', required=True)
+    parser.add_argument('--embed_separately', dest='embed_separately', default=False, action='store_true',
+                        help='whether to embed the query and code separately or concatenated together')
+    parser.add_argument('--version', dest='version', type=str,
+                        help='how to compute query embedding, can be CLS or MEAN', required=True)
+
     args = parser.parse_args()
+    checkpoints = []
+    assert (args.checkpoint or args.checkpoint_dir), \
+        "Either --load_from_checkpoint or --checkpoint_dir must be specified"
+    assert (not (args.checkpoint and args.checkpoint_dir)), \
+        "Only one from --load_from_checkpoint or --checkpoint_dir can be specified"
+
+    if args.checkpoint is not None:
+        checkpoints.append(args.checkpoint)
+    elif args.checkpoint_dir:
+        import glob
+        all_checkpoints = glob.glob(args.checkpoint_dir + '/*')
+        for checkpoint in all_checkpoints:
+            assert checkpoint.endswith('.tar')
+            checkpoints.append(checkpoint)
 
     device = args.device
-    embedder = Embedder(device, model_eval=True)
-    scorer = torch.nn.Sequential(torch.nn.Linear(embedder.get_dim() * 2, embedder.get_dim()),
-                                 torch.nn.ReLU(),
-                                 torch.nn.Linear(embedder.get_dim(), 1)).to(device)
-    print(f"Evaluating checkpoint {args.checkpoint}")
-    models = torch.load(args.checkpoint, map_location=device)
-    scorer.load_state_dict(models['scorer'])
-    scorer = scorer.to(device)
-    embedder.model.load_state_dict(models['embedder'])
-    embedder.model = embedder.model.to(device)
+    version = args.version
+    embed_separately = args.embed_separately
+    valid_data = pd.read_json(args.valid_file_name, lines=True)
 
-    train_data = pd.read_json(args.train_file_name, lines=True)
-    print("Searching for best split point on train data....")
     with torch.no_grad():
-        split_points, f1_scores = find_split_point(train_data[:50], scorer, embedder)
-        split_point = split_points[np.argmax(f1_scores)]
-        print(f"Search complete, will be using split_point={split_point}")
-        valid_data = pd.read_json(args.valid_file_name, lines=True)
-        print("Running evaluation....")
-        f1_scores, precisions, recalls = run_eval_epoch(valid_data, scorer, embedder, split_point=split_point)
-        print(f"Mean precision: {np.mean(precisions)}, mean recall: {np.mean(recalls)}, mean F1: {np.mean(f1_scores)}")
+        for checkpoint in checkpoints:
+            embedder = Embedder(device, model_eval=True)
+            scorer = torch.nn.Sequential(torch.nn.Linear(embedder.get_dim() * 2, embedder.get_dim()),
+                                         torch.nn.ReLU(),
+                                         torch.nn.Linear(embedder.get_dim(), 1)).to(device)
+
+            print(f"Evaluating checkpoint {checkpoint}")
+            models = torch.load(checkpoint, map_location=device)
+            scorer.load_state_dict(models['scorer'])
+            scorer = scorer.to(device)
+            embedder.model.load_state_dict(models['embedder'])
+            embedder.model = embedder.model.to(device)
+
+            print("Searching for best split point on valid data....")
+            split_points, f1_scores = find_split_point(valid_data[:50], scorer, embedder, embed_separately, version)
+            split_point = split_points[np.argmax(f1_scores)]
+            print(f"Search complete, will be using split_point={split_point}")
+            print("Running evaluation....")
+            f1_scores, precisions, recalls = run_eval_epoch(valid_data, scorer, embedder, embed_separately, version,
+                                                            split_point=split_point)
+            print(f"Mean precision: {np.mean(precisions)}, ", f"mean recall: {np.mean(recalls)}, ",
+                  f"mean F1: {np.mean(f1_scores)}")
 
 
 if __name__ == '__main__':
--- scoring/eval_utils.py
+++ scoring/eval_utils.py
@@ -2,7 +2,7 @@ import bleach
 import numpy as np
 import torch
 
-from scoring.utils import extract_noun_tokens, get_ground_truth_matches
+from scoring.utils import get_noun_phrases, get_ground_truth_matches, embed_pair
 
 
 class TokenVal(object):
@@ -46,13 +46,39 @@ def get_tokenvals(tokens, scores, idxs):
     return token_vals
 
 
-def generate_HTML(title, embedder, tokens, ground_truth_idxs, scores, split_point):
+def generate_HTML(title, embedder, tokens, ground_truth_idxs, scores, split_point, color):
     predicted_idxs = np.where(scores >= split_point)[0]
 
     true_scores_tvs = get_tokenvals(tokens, np.ones(len(tokens)), ground_truth_idxs)
     predicted_scores_tvs = get_tokenvals(tokens, scores, predicted_idxs)
-    return '<h2>{0}</h2>{1}{2}'.format(title, tokenvals_to_html(embedder, predicted_scores_tvs, 0),
+    return '<h2>{0}</h2>{1}{2}'.format(title, tokenvals_to_html(embedder, predicted_scores_tvs, color),
                                        tokenvals_to_html(embedder, true_scores_tvs, 1))
+
+
+def normalize_predictions(scorer_out, scorer, embedder, code, embed_separately, version):
+    random_words = ['gate', 'version', 'average', 'logic']
+    scores_for_random_words = []
+    for word in random_words:
+        out_tuple = embed_pair(embedder, word, code, embed_separately)
+        scores_for_random_words.append(scorer_forward(scorer, out_tuple, version))
+    scores_for_random_words = np.asarray(scores_for_random_words)
+    new_scores = (scorer_out - scores_for_random_words)
+    return new_scores
+
+
+def scorer_forward(scorer, embedder_out, version='CLS'):
+    _, word_token_embs, code_token_id_mapping, code_embs, __, truncated_code_tokens, cls_token_emb = embedder_out
+    if version == 'CLS':
+        emb = cls_token_emb
+    elif version == 'MEAN':
+        emb = torch.mean(word_token_embs, dim=0, keepdim=True)
+    else:
+        raise Exception("Unknown average computing version!")
+    tiled_emb = emb.repeat(len(truncated_code_tokens), 1)
+    forward_input = torch.cat((tiled_emb, code_embs), dim=1)
+    token_count = max(code_token_id_mapping[-1])
+    scorer_out = torch.sigmoid(scorer.forward(forward_input)).squeeze().cpu().detach().numpy()[:token_count]
+    return scorer_out
 
 
 def compute_f1(ground_truth_idxs, predicted_idxs):
@@ -72,7 +98,8 @@ def compute_f1(ground_truth_idxs, predicted_idxs):
     return f1_score, P_t, R_t
 
 
-def eval_example(data, it, scorer, embedder, evaluate, split_point=0.5):
+def eval_example(data, it, scorer, embedder, evaluate, embed_separately=False, split_point=0.5, version="CLS",
+                 color=[0]):
     result_dict = {}
     if evaluate == "F1":
         result_dict['f1_scores_for_sample'] = []
@@ -83,40 +110,40 @@ def eval_example(data, it, scorer, embedder, evaluate, split_point=0.5):
     else:
         raise Exception("Invalid value of evaluate parameter")
 
-    doc = data['docstring_tokens'][it]
     code = data['alt_code_tokens'][it]
     static_tags = data['static_tags'][it]
     regex_tags = data['regex_tags'][it]
-    noun_tokens = extract_noun_tokens(' '.join(doc))
+    ccg_parse = data['ccg_parse'][it]
 
-    out_tuple = embedder.embed_and_filter(doc, code, noun_tokens)
-    if out_tuple is None:
-        if evaluate == "F1":
-            result_dict['f1_scores_for_sample'] = [0]
-            result_dict['pre_for_sample'] = [0]
-            result_dict['re_for_sample'] = [0]
-        elif evaluate == "HTML":
-            result_dict['html'] = []
+    if len(ccg_parse) == 0 or '\\' in ccg_parse:
+        # TODO: this example is not parsed properly, skip for now, but handle somehow in the future
         return result_dict
-    noun_token_id_mapping, noun_token_embeddings, code_token_id_mapping, code_embedding, _, truncated_code_tokens = out_tuple
-    for nti, nte, nt in zip(noun_token_id_mapping, noun_token_embeddings, noun_tokens):
-        nte = nte.unsqueeze(0)
-        # check for regex and static matches
-        ground_truth_idxs = get_ground_truth_matches(nt, code, code_token_id_mapping, static_tags, regex_tags)
-        ground_truth_idxs = np.unique(ground_truth_idxs)
-        # forward pass through scorer
-        tiled_nte = nte.repeat(len(truncated_code_tokens), 1)
-        forward_input = torch.cat((tiled_nte, code_embedding), dim=1)
-        token_count = max(code_token_id_mapping[-1])
-        scorer_out = torch.sigmoid(scorer.forward(forward_input)).squeeze().cpu().detach().numpy()[:token_count]
 
+    phrases = get_noun_phrases(ccg_parse)
+    for phrase in phrases:
+        out_tuple = embed_pair(embedder, phrase, code, embed_separately)
+        if out_tuple is None:
+            if evaluate == "F1":
+                result_dict['f1_scores_for_sample'] = []
+                result_dict['pre_for_sample'] = []
+                result_dict['re_for_sample'] = []
+            elif evaluate == "HTML":
+                result_dict['html'] = []
+            return result_dict
+        _, noun_token_embs, code_token_id_mapping, code_embs, __, truncated_code_tokens, cls_token_emb = out_tuple
+        ground_truth_idxs_for_phrase = []
+        for token in phrase:
+            ground_truth_idxs = get_ground_truth_matches(token, code, code_token_id_mapping, static_tags, regex_tags)
+            ground_truth_idxs_for_phrase.extend(ground_truth_idxs)
+        ground_truth_idxs_for_phrase = np.unique(ground_truth_idxs_for_phrase)
+        scorer_out = scorer_forward(scorer, out_tuple, version)
         predicted_idxs = np.where(scorer_out > split_point)[0]
-
         if evaluate == "F1":
             # this is equivalent to maxpooling
             reversed_code_token_id_mapping = {ai: i for i, a in enumerate(code_token_id_mapping) for ai in a}
             orig_token_predicted_idx = np.unique([reversed_code_token_id_mapping[idx] for idx in predicted_idxs])
-            orig_token_ground_truth_idx = np.unique([reversed_code_token_id_mapping[idx] for idx in ground_truth_idxs])
+            orig_token_ground_truth_idx = np.unique(
+                [reversed_code_token_id_mapping[idx] for idx in ground_truth_idxs_for_phrase])
             out = compute_f1(orig_token_ground_truth_idx, orig_token_predicted_idx)
             if out is None:
                 continue
@@ -127,11 +154,13 @@ def eval_example(data, it, scorer, embedder, evaluate, split_point=0.5):
                 result_dict['re_for_sample'].append(re)
         elif evaluate == "HTML":
             result_dict['html'].append(
-                generate_HTML(nt, embedder, truncated_code_tokens, ground_truth_idxs, scorer_out, split_point))
+                generate_HTML(' '.join(phrase), embedder, truncated_code_tokens, ground_truth_idxs_for_phrase,
+                              scorer_out,
+                              split_point, color))
     return result_dict
 
 
-def find_split_point(data, scorer, embedder):
+def find_split_point(data, scorer, embedder, embed_separately, version):
     avg_f1 = []
     split_points = np.arange(0, 1, 0.05)
     for i in split_points:
@@ -139,7 +168,8 @@ def find_split_point(data, scorer, embedder):
         precisions = []
         recalls = []
         for it in range(len(data)):
-            result_dict = eval_example(data, it, scorer, embedder, evaluate="F1", split_point=i)
+            result_dict = eval_example(data, it, scorer, embedder, evaluate="F1", split_point=i,
+                                       embed_separately=embed_separately, version=version)
             f1 = result_dict['f1_scores_for_sample']
             pre = result_dict['pre_for_sample']
             re = result_dict['re_for_sample']
