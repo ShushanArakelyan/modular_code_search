25729cfe7635efe48a841cf3df845d05aceb671c
ShushanArakelyan <shushana@usc.edu>
2021-10-06 21:24:48 -0700

    fixing bug in codebert oracle score generation process

--- eval/codebert_oracle_scores.py
+++ eval/codebert_oracle_scores.py
@@ -21,7 +21,7 @@ data = pd.read_json(
 neg_data = pd.read_json(
     '/home/shushan/datasets/CodeSearchNet/resources/ccg_parses_only/python/final/jsonl/train/ccg_train_5.jsonl.gz',
     lines=True)
-embedder.init_embedder('cuda:0')
+embedder.init_embedder(device)
 
 range_start = sys.argv[1]
 range_end = sys.argv[2]
@@ -45,7 +45,7 @@ for idx in tqdm.tqdm(range(int(range_start), int(range_end))):
     np.random.seed(idx)
     negative_sample_idxs = []
     for _ in range(neg_count):
-        random_idx = np.random.randint(0, len(neg_data), 1)[0]
+        random_idx = np.random.randint(1, len(neg_data), 1)[0]
         negative_sample_idxs.append(random_idx)
         sample = (data['docstring_tokens'][idx],
                   neg_data['code_tokens'][random_idx],
@@ -54,7 +54,8 @@ for idx in tqdm.tqdm(range(int(range_start), int(range_end))):
                   data['ccg_parse'][idx])
         all_samples.append(sample)
         docs.append(' '.join(neg_data['code_tokens'][random_idx]))
-
+    
+    print("negative sample idxs: ", negative_sample_idxs)
     preds = None
     with torch.no_grad():
         for s in all_samples:
@@ -67,7 +68,8 @@ for idx in tqdm.tqdm(range(int(range_start), int(range_end))):
             else:
                 preds = np.append(preds, logits.cpu().numpy(), axis=0)
     top_idxs = np.argsort(preds[:, 1])[::-1][:10]
-    oracle_scores.append(negative_sample_idxs[top_idxs])
+    print("hardest examples: ", [negative_sample_idxs[i] for i in top_idxs])
+    oracle_scores.append([negative_sample_idxs[i] for i in top_idxs])
     
 with open(f'/home/shushan/oracle_scores_{range_start}_{range_end}.txt', 'w') as f:
     for scores in oracle_scores:
