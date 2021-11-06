5a63344f39499c626201f81cbf9243df8eaa66c8
ShushanArakelyan <shushana@usc.edu>
2021-11-05 17:34:23 -0700

    small bug fixes in version 7.1

--- layout_assembly/action_v7_1.py
+++ layout_assembly/action_v7_1.py
@@ -38,12 +38,19 @@ class ActionModule_v7_1_one_input(ActionModule_v5):
         code_embeddings = torch.cat((code_embeddings, padding_zeros), dim=1)
         encoder_input = torch.cat((tiled_verb_emb, tiled_prep_emb, scores[:seq_len], code_embeddings), dim=1)
         encoder_input = torch.cat((tiled_cls_emb, encoder_input, tiled_sep_emb), dim=0).unsqueeze(dim=1)
-        mlp_input = self.encoder_layer(encoder_input)
-        mlp_input = torch.index_select(mlp_input, 0, torch.LongTensor(range(1, seq_len)).to(self.device)).squeeze()
+        print("encoder input before padding: ", encoder_input.shape)
+        encoder_input = torch.nn.functional.pad(encoder_input, (0, 0, 0, 0, 0, embedder.max_seq_length - encoder_input.shape[0]),
+                                                 'constant', 0)
+        print("encoder input after padding: ", encoder_input.shape)
+        mlp_input = self.encoder_layer(encoder_input).squeeze()
+        print("mlp input: ", mlp_input.shape)
+#         mlp_input = torch.index_select(mlp_input, 0, torch.LongTensor(range(1, seq_len)).to(self.device)).squeeze()
         mlp_input = torch.mm(scores.T, mlp_input)
+        print("mlp input: ", mlp_input.shape)
         scores_out = self.mlp.forward(mlp_input).T
-        scores_out = torch.nn.functional.pad(scores_out, (0, 0, 0, embedder.max_seq_length - scores_out.shape[0]),
-                                             'constant', 0)
+#         scores_out = torch.nn.functional.pad(scores_out, (0, 0, 0, embedder.max_seq_length - scores_out.shape[0]),
+#                                              'constant', 0)
+        print("scores_out: ", scores_out.shape)
         l1_reg_loss = torch.norm(scores_out, 1)
         return None, scores_out, l1_reg_loss
 
@@ -54,7 +61,7 @@ class ActionModule_v7_1_two_inputs(ActionModule_v5):
         self.input_dim = 3080
         self.padding_size = self.input_dim - (embedder.dim * 4 + 2)
         self.encoder_layer = nn.TransformerEncoderLayer(d_model=self.input_dim, nhead=8).to(self.device)
-        hidden_input_dims = [self.input_dim, 512]
+        hidden_input_dims = [self.input_dim * 2, 512]
         hidden_output_dims = [512, 512]
         if self.normalized:
             self.mlp = FC2_normalized(hidden_input_dims, hidden_output_dims, dropout=self.dropout).to(self.device)
@@ -92,10 +99,10 @@ class ActionModule_v7_1_two_inputs(ActionModule_v5):
             dim=1)
         encoder_input = torch.cat((tiled_cls_emb, encoder_input, tiled_sep_emb), dim=0).unsqueeze(dim=1)
         print("encoder input before padding: ", encoder_input.shape)
-        encoder_input = torch.nn.functional.pad(encoder_input, (0, 0, 0, embedder.max_seq_length - encoder_input.shape[0]),
+        encoder_input = torch.nn.functional.pad(encoder_input, (0, 0, 0, 0, 0, embedder.max_seq_length - encoder_input.shape[0]),
                                              'constant', 0)
         print("encoder input after padding: ", encoder_input.shape)
-        mlp_input = self.encoder_layer(encoder_input)
+        mlp_input = self.encoder_layer(encoder_input).squeeze()
         print("mlp input: ", mlp_input.shape)
         # mlp_input = torch.index_select(mlp_input, 0, torch.LongTensor(range(1, seq_len)).to(self.device)).squeeze()
         mlp_input_1 = torch.mm(scores1.T, mlp_input)
