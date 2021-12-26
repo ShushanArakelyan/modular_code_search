5181566a39f3d3f0a781c6c4baf404564576f22c
shushan <arakelyan.shushan@gmail.com>
2021-12-26 09:54:22 -0800

    bug fix

--- layout_assembly/main2.py
+++ layout_assembly/main2.py
@@ -39,10 +39,10 @@ def make_prediction(output_list):
         print("output_list[i][0]: ", output_list[i][0].shape, ", output_list[i][1]: ", output_list[i][1].shape)
         s = torch.sigmoid(torch.dot(output_list[i][0].squeeze(), output_list[i][1].squeeze()))
         if alignment_scores is None:
-            alignment_scores = s
+            alignment_scores = s.unsqueeze(0)
             print("alignment_scores.shape: ", alignment_scores.shape)
         else:
-            alignment_scores = torch.cat((alignment_scores.unsqueeze(dim=0), s.unsqueeze(dim=0)))
+            alignment_scores = torch.cat((alignment_scores, s.unsqueeze(dim=0)))
             print("alignment_scores.shape: ", alignment_scores.shape)
     pred = torch.prod(alignment_scores)
     return pred
