b19857aa7226488a0cf66827cc02c702da90ea19
shushan <arakelyan.shushan@gmail.com>
2021-12-26 09:47:37 -0800

    update make prediction to handle multiple scores

--- layout_assembly/main2.py
+++ layout_assembly/main2.py
@@ -33,10 +33,16 @@ def compute_alignment(a, b):
 
 
 def make_prediction(output_list):
-    output_tensor = torch.cat(output_list[0], dim=1)
-    for i in range(1, len(output_list)):
-        output_tensor = torch.cat((output_tensor, *output_list[i]), dim=1)
-    alignment_scores = torch.sigmoid(torch.dot(output_tensor[:, 0], output_tensor[:, 1]))
+    alignment_scores = None
+    print("len of output_list: ", len(output_list))
+    for i in range(len(output_list)):
+        print("output_list[i][0]: ", output_list[i][0].shape, ", output_list[i][1]: ", output_list[i][1].shape)
+        if alignment_scores is None:
+            alignment_scores = torch.sigmoid(torch.dot(output_list[i][0], output_list[i][1]))
+            print("alignment_scores.shape: ", alignment_scores.shape)
+        else:
+            alignment_scores = torch.cat((alignment_scores, torch.sigmoid(torch.dot(output_list[i][0], output_list[i][1]))))
+            print("alignment_scores.shape: ", alignment_scores.shape)
     pred = torch.prod(alignment_scores)
     return pred
 
