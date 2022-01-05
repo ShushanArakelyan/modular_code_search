44514ccd1d651277d509d1c8a7d51e7de8e9fbca
shushan <arakelyan.shushan@gmail.com>
2022-01-04 21:12:19 -0800

    add debug info

--- layout_assembly/main2.py
+++ layout_assembly/main2.py
@@ -38,7 +38,9 @@ def make_prediction(output_list):
     alignment_scores = None
     for i in range(len(output_list)):
         s = torch.dot(output_list[i][0].squeeze(), output_list[i][1].squeeze())
-        # s /= sum(output_list[i][0]).squeeze()
+        print("before normalization: ", s)
+        s /= sum(output_list[i][0]).squeeze()
+        print("after normalization: ", s)
         if alignment_scores is None:
             alignment_scores = s.unsqueeze(0)
         else:
