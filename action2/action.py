removing bug and fixing masking index issue

action2/action.py
@@ -34,31 +34,30 @@ class ActionModule(object):
     def forward(self, inputs, masking_indx, precomputed_embeddings):
         updated_inputs = []
         num_unmasked_inputs = len(inputs) - 1
-        print("Num unmasked inputs: ", num_unmasked_inputs)
+        # print("Num unmasked inputs: ", num_unmasked_inputs)
         if num_unmasked_inputs > self.max_inputs_allowed - 1 or precomputed_embeddings is None:
             raise ProcessingException()
         verb_embedding, code_embedding = precomputed_embeddings
-        masking_indx = min(masking_indx, num_unmasked_inputs)
-        print("Masking indx: ", masking_indx)
+
+        # check if there are any action arguments, which we don't include as argument
         for indx, i in enumerate(inputs):
             if len(i) < 2:
                 num_unmasked_inputs -= 1
-                masking_indx = min(masking_indx, num_unmasked_inputs)
-                print("New masking indx: ", masking_indx)
-                # we are skipping some arguments, e.g. action-s, so it is possible
-                # to have a preposition without its corresponding scores
                 continue
+        masking_indx = min(masking_indx, num_unmasked_inputs)
+        # print("Masking indx: ", masking_indx)
+        for indx, i in enumerate(inputs):
             prep_embedding, scores = i
             if isinstance(scores, tuple):
                 _, scores = scores
             if len(scores.shape) == 1:
                 scores = scores.unsqueeze(dim=1)
             if indx == masking_indx:
-                print("Masking current idx: ", indx)
+                # print("Masking current idx: ", indx)
                 # mask this index
                 true_scores = scores
                 if not np.any(true_scores.detach().cpu().numpy()):
-                    print("all output scores are zeros")
+                    # print("all output scores are zeros")
                 scores = torch.zeros_like(scores).to(self.device)
             fwd_input = torch.cat((verb_embedding, prep_embedding), dim=1)
             out = self.verb_embedder(fwd_input)