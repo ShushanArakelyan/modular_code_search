eddd8106d83fa1741df17b6995fe1d30ee339f72
shushan <arakelyan.shushan@gmail.com>
2022-01-08 10:18:02 -0800

    remove debug info

--- layout_assembly/layout_ws2.py
+++ layout_assembly/layout_ws2.py
@@ -57,7 +57,7 @@ class LayoutNetWS2(LayoutNet):
         tree = self.construct_layout(ccg_parse)
         tree = self.remove_concats(tree)
         code = sample[1]
-        print("new example: ", ccg_parse)
+        # print("new example: ", ccg_parse)
         if len(code) == 0:  # erroneous example
             raise ProcessingException()
         scoring_inputs, verb_embeddings = self.precompute_inputs(tree, code, [[], [], []], [[], []], '')
@@ -94,7 +94,7 @@ class LayoutNetWS2(LayoutNet):
             if precomputed_embeddings[0].shape[0] == 0 or precomputed_embeddings[1].shape[0] == 0:
                 raise ProcessingException()
             action_it += 1
-            print("Num of inputs passed to action: ", len(action_module_wrapper.inputs))
+            # print("Num of inputs passed to action: ", len(action_module_wrapper.inputs))
             outputs = self.action_module.forward(action_module_wrapper.inputs, self.get_masking_idx(),
                                                  precomputed_embeddings)
             output_list.append(outputs)
