41be2a670b4e5672355bfc8f362fd69637df4733
shushan <arakelyan.shushan@gmail.com>
2022-03-31 19:24:07 +0400

    fix typo

--- layout_assembly/main2.py
+++ layout_assembly/main2.py
@@ -334,7 +334,7 @@ def main(device, data_dir, scoring_checkpoint, num_epochs, num_epochs_pretrainin
         print(f"Loading all files from dataset from {data_dir}")
         d_list = []
         for file in glob.glob(os.path.join(data_dir, "/*")):
-            print(f"Loading dataset from {data_dir}")
+            print(f"Loading dataset from {file}")
             d_list.extend([CodeSearchNetDataset_NotPrecomputed(file, device), ] +
                                     [CodeSearchNetDataset_NotPrecomputed_RandomNeg(filename=file, device=device,
                                                                                    range=r) for r in range(1)])
